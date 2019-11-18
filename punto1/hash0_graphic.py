7f60698d391b2fdfb7c9e13d576d4f2d5589379b
manuacostaf18 <55296684+manuacostaf18@users.noreply.github.com>
2019-11-18 14:53:57 -0500

    Add files via upload

---
+++ punto1/hash0_graphic.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as pp
+
+archivo = open("hash0.txt")
+eje_y = [int(i) for i in archivo.read().split(",")[:-1]]
+archivo.close()
+
+eje_x = [n for n in range(101)]
+pp.bar(eje_x, eje_y, color = 'blue')
+pp.title("Hash0 - Distrubition")
+pp.xlabel("Posición")
+pp.ylabel("Número de elementos")
+
+pp.show()
+
